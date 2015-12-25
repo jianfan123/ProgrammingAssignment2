8adb1657359e73af5accbfa339ddc9f0bff886b5
fanj <jian.fan@ask.com>
2015-12-25 12:31:01 -0500

    second commit

--- cachematrix.R
+++ cachematrix.R
@@ -38,8 +38,3 @@ cacheSolve<- function(x,...) {
 
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
